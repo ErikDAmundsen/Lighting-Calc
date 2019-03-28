c530edebab87e0059e1af4b0d57d91bf984c25c0
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-03-28 13:49:25 -0700

    first commit

---
+++ Methods.js
@@ -0,0 +1,62 @@
+// // populate brand dropdown
+// $(document).ready(function () {
+ 
+//     var brandList = document.getElementById('brand');
+     
+//     brandList.options[0] = new Option('--Select--', '');
+//     brandList.options[1] = new Option('Snacks', 'Snacks');
+//     brandList.options[2] = new Option('Drinks', 'Drinks');
+//     });
+//     // populate model drop down
+//     function populateModelList(){
+ 
+//         var brandList = document.getElementById('brand');
+//         var modelList = document.getElementById("model");
+//         var selectedBrand = brandList.options[brandList.selectedIndex].value;
+         
+//         if (selectedBrand=='Snacks')
+//         {
+             
+//             modelList.options.length=0;
+//             modelList.options[0] = new Option('--Select--', '');
+//             modelList.options[1] = new Option('Burger', 'Burger');
+//             modelList.options[2] = new Option('Pizza', 'Pizza');
+//             modelList.options[3] = new Option('Hotdog', 'Hotdog');
+//             modelList.options[4] = new Option('Potato Chips', 'Potato Chips');
+//             modelList.options[5] = new Option('French Fries', 'French Fries');
+             
+//         }
+//         else if (list1SelectedValue=='Drinks')
+//         {
+             
+//             modelList.options.length=0;
+//             modelList.options[0] = new Option('--Select--', '');
+//             modelList.options[1] = new Option('Coca Cola', 'Coca Cola');
+//             modelList.options[2] = new Option('7up', '7up');
+//             modelList.options[3] = new Option('Pepsi', 'Pepsi');
+//             modelList.options[4] = new Option('Coffee', 'Coffee');
+//             modelList.options[5] = new Option('Tea', 'Tea');
+             
+//         }
+// }
+
+//populate brand drop down
+$(document).ready(function () {
+    $.each(Brands, function (Brand,obj){
+        $("#brand").append($('<option></option>').val(obj.Brand).html(obj.Brand));
+    
+    });
+  
+   
+// // populate model drop down
+//      populateModelList(){
+
+//         var selectedBrand = brandList.options[brandList.selectedIndex].value;
+        
+//         if (selectedBrand== val('obj.Brand'))
+//         {
+
+//         }
+
+//     } 
+//  });
---
+++ Objects.js
@@ -0,0 +1,51 @@
+var Brands= [ 
+        { "Brand":"Arri",
+            "Models": [ { "Model": "M18 1200w 1- Spot",
+                        "Angle":15,
+                        "FC": 2700,
+                        "Diameter":1.32},
+                       
+                        { "Model": "Skypanel S120-C(Tungsten)",
+                        "Angle":106,
+                        "FC": 488,
+                        "Diameter":13.27},
+                        
+                        { "Model": "Skypanel S120-C(Daylight)",
+                        "Angle":106,
+                        "FC": 448,
+                        "Diameter":13.27}
+                    ]
+
+          },
+        { "Brand":"Astera",
+         "Models":[ { Model: "Pixel Tube Tungsten",
+                Angle:180,
+                FC: 10,
+                Diameter:14.6},
+               
+                { Model: "Pixel Tube Daylight",
+                Angle:180,
+                FC: 10,
+                Diameter:14.6}]
+        },
+
+        { "Brand":"Digital Sputnik Tungsten",
+         "Models":[ { Model: "DS1 Daylight",
+                Angle:20,
+                FC: 1336,
+                Diameter:null},
+               
+                { Model: "DS1 Tungsten",
+                Angle:20,
+                FC: 1336,
+                Diameter:null}]
+        }
+
+
+
+
+
+        ]
+
+
+

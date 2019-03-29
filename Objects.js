3fc1982c49e539f38ad3767cf7e33ad1b0a4c165
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-03-29 13:04:47 -0700

    both dropdowns autopopulate

--- Objects.js
+++ Objects.js
@@ -1,51 +1,68 @@
-var LightList= [ 
-        { "Brand":"Arri",
-            "Models": [ { "Model": "M18 1200w 1- Spot",
-                        "Angle":15,
+var LightList = [
+        {
+                "Brand": "Arri",
+                "Models": [{
+                        "Name": "M18 1200w 1- Spot",
+                        "Angle": 15,
                         "FC": 2700,
-                        "Diameter":1.32},
-                       
-                        { "Model": "Skypanel S120-C(Tungsten)",
-                        "Angle":106,
-                        "FC": 488,
-                        "Diameter":13.27},
-                        
-                        { "Model": "Skypanel S120-C(Daylight)",
-                        "Angle":106,
-                        "FC": 448,
-                        "Diameter":13.27}
-                    ]
+                        "Diameter": 1.32
+                },
 
-          },
-        { "Brand":"Astera",
-         "Models":[ { Model: "Pixel Tube Tungsten",
-                Angle:180,
-                FC: 10,
-                Diameter:14.6},
-               
-                { Model: "Pixel Tube Daylight",
-                Angle:180,
-                FC: 10,
-                Diameter:14.6}]
+                {
+                        "Name": "Skypanel S120-C(Tungsten)",
+                        "Angle": 106,
+                        "FC": 488,
+                        "Diameter": 13.27
+                },
+
+                {
+                        "Name": "Skypanel S120-C(Daylight)",
+                        "Angle": 106,
+                        "FC": 448,
+                        "Diameter": 13.27
+                }
+                ]
+
+        },
+        {
+                "Brand": "Astera",
+                "Models": [{
+                        "Name": "Pixel Tube Tungsten",
+                        "Angle": 180,
+                        "FC": 10,
+                        "Diameter": 14.6
+                },
+
+                {
+                        "Name": "Pixel Tube Daylight",
+                        "Angle": 180,
+                        "FC": 10,
+                        "Diameter": 14.6
+                }]
         },
 
-        { "Brand":"Digital Sputnik",
-         "Models":[ { Model: "DS1 Daylight",
-                Angle:20,
-                FC: 1336,
-                Diameter:null},
-               
-                { Model: "DS1 Tungsten",
-                Angle:20,
-                FC: 1336,
-                Diameter:null}]
+        {
+                "Brand": "Digital Sputnik",
+                "Models": [{
+                        "Name": "DS1 Daylight",
+                        "Angle": 20,
+                        "FC": 1336,
+                        "Diameter": null
+                },
+
+                {
+                        "Name": "DS1 Tungsten",
+                        "Angle": 20,
+                        "FC": 1336,
+                        "Diameter": null
+                }]
         }
 
 
 
 
 
-        ]
+]
 
 
 

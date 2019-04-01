05906ccb56004d84980ee3519004c717ca7f80e0
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-04-01 16:45:58 -0700

    sliders work, database2.0 formed

--- methods.js
+++ methods.js
@@ -1,52 +1,7 @@
-// // populate brand dropdown
-// $(document).ready(function () {
 
-//     var brandList = document.getElementById('brand');
-
-//     brandList.options[0] = new Option('--Select--', '');
-//     brandList.options[1] = new Option('Snacks', 'Snacks');
-//     brandList.options[2] = new Option('Drinks', 'Drinks');
-//     });
-//     // populate model drop down
-//     function populateModelList(){
-
-//         var brandList = document.getElementById('brand');
-//         var modelList = document.getElementById("model");
-//         var selectedBrand = brandList.options[brandList.selectedIndex].value;
-
-//         if (selectedBrand=='Snacks')
-//         {
-
-//             modelList.options.length=0;
-//             modelList.options[0] = new Option('--Select--', '');
-//             modelList.options[1] = new Option('Burger', 'Burger');
-//             modelList.options[2] = new Option('Pizza', 'Pizza');
-//             modelList.options[3] = new Option('Hotdog', 'Hotdog');
-//             modelList.options[4] = new Option('Potato Chips', 'Potato Chips');
-//             modelList.options[5] = new Option('French Fries', 'French Fries');
-
-//         }
-//         else if (list1SelectedValue=='Drinks')
-//         {
-
-//             modelList.options.length=0;
-//             modelList.options[0] = new Option('--Select--', '');
-//             modelList.options[1] = new Option('Coca Cola', 'Coca Cola');
-//             modelList.options[2] = new Option('7up', '7up');
-//             modelList.options[3] = new Option('Pepsi', 'Pepsi');
-//             modelList.options[4] = new Option('Coffee', 'Coffee');
-//             modelList.options[5] = new Option('Tea', 'Tea');
-
-//         }
-// }
 
 //populate brand drop down
-
-
 $(document).ready(function () {
-
-
-
 
     LightList.forEach(function (BrandName) {
         var newOption = document.createElement("option");
@@ -55,73 +10,58 @@ $(document).ready(function () {
         newOption.innerHTML = BrandName.Brand;
         BrandList.appendChild(newOption);
 
-
-
-
-
-
     })
-
 })
-
-
-
 
 // //    populate model drop down
 
 function populateModelList() {
+
     var BrandList = document.getElementById('brand');
+var ModelList = document.getElementById('model'); 
+ModelList.innerHTML = "<option value='' selected disabled hidden>Choose a model</option>";   
 var selectedBrand = BrandList.value;
 var  BrandContents = LightList.filter(modelName => modelName.Brand == selectedBrand);
         BrandContents[0]["Models"].forEach(function(model){
 var newOption = document.createElement("option");
-var ModelList = document.getElementById('model');
+
+
 newOption.innerHTML = model.Name;
-ModelList.appendChild(newOption)
+ModelList.appendChild(newOption);
 
         })
-         
-         
-     
-
-   
-    
-     
-  
-     
 };
+//distance slider
+var DistSlider = document.getElementById("DistSlider");
+DistSlider.oninput = function(){
+
+var DistOutput =document.getElementById("DistOP");
+DistOutput.innerHTML = DistSlider.value;
+
+    DistOutput.innerHTML=this.value;
+};
+//diameter slider
+
+var DiameterSlider = document.getElementById("DMTSlider");
+DiameterSlider.oninput = function() {
+
+var DiameterOutput = document.getElementById("DMTOP");
+DiameterOutput.innerHTML= DiameterSlider.value;  
+  DiameterOutput.innerHTML=this.value;
+}
+//Fooot candle slider
+
+FCSlider.oninput =function(){
+var FCSlider = document.getElementById("FCSlider");
+var FCOutput = document.getElementById("FCOP");
+FCOutput.innerHTML = FCSlider.value; 
+   FCOutput.innerHTML=this.value;
+}
+// document.getElementById("DistSlider").addEventListener("input",upateSliders(this.value) );
 
 
-
-// // forEach(function)
-// //     }
-
-    //  for (var i = 0; i < LightList.length; i++) {
-    //    ele.innerHTML = ele.innerHTML + '<option value=' + LightList[i]['Brand'] + '</option';
-    //  }
-
-
-
-
-//     $.each(LightList, function (Brand,obj){
-//         $("#brand").append($('<option></option>').val(obj.Brand).html(obj.Brand));
-
-//     });
-
-//   });  
-
-// // populate model drop down
-//     function populateModelList(){
-
-//         var brandList = document.getElementById('brand');
-//         var modelList = document.getElementById("model");
-
-//         var selectedBrand = brandList.value;   
-//       var model= LightList.Brand[selectedBrand]
-
-
-//         $.each(Model,  function(Model,obj){
-//            $("#model").append($('<option></option>').val(obj.model).html(obj.Model))
-//        })
-
-
+function upateSliders(value)
+{
+    document.getElementById("DMTSlider").value = value * 2;
+     document.getElementById("DMTOP").innerHTML = value * 2;
+};

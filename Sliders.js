330c0d0099c7e882d321782304561546ab8b001a
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-04-04 15:32:19 -0700

    targeting of all elements work

--- Sliders.js
+++ Sliders.js
@@ -1,37 +1,69 @@
-//Masster update function
-function upateSliders(value) {
-    document.getElementById("DMTSlider").value = value * 2;
-    document.getElementById("DMTOP").innerHTML = value * 2;
-};
+//global variables
 var DistSlider = document.getElementById("DistSlider");
- var DistOutput = document.getElementById("DistOP");
-var DiameterSlider = document.getElementById("DMTSlider"); 
+var DistOutput = document.getElementById("DistOP");
+var DiameterSlider = document.getElementById("DMTSlider");
 var DiameterOutput = document.getElementById("DMTOP");
 var FCSlider = document.getElementById("FCSlider");
 var FCOutput = document.getElementById("FCOP");
+
+// reusable function to get object of selected model
+function GetCurrentModel(){
+var ModelList = document.getElementById('model');
+ currentModel = ModelList.value;
+ var SelectedModel = FullSpecs.filter(Mix => $.trim(Mix["Model"]) == currentModel);
+ return SelectedModel[0];
+}
+
+//assign slider values and Beam angle to start
+function fillValues(){
+
+var SM = GetCurrentModel();
+var BeamA = document.getElementById("BeamAngle");
+BeamA.innerHTML= SM["Beam Angle"]
+DistSlider.value = 5;
+DistOutput.innerHTML = 5;
+DiameterSlider.value = SM["Spot Diameter @ 5'"];
+DiameterOutput.innerHTML = SM["Spot Diameter @ 5'"];
+FCSlider.value = SM["FC @ 5’ (Daylight)"];
+FCOutput.innerHTML = SM["FC @ 5’ (Daylight)"];
+ 
+}
+
+//Master update function
+function upateSliders(value) {
+
+    updateDistSlider();
+    updateDiaSlider();
+    upadteFCSlider();
+
+};
+
+function updateDistSlider(){
+
+    
+}
 //distance slider
 
 DistSlider.oninput = function () {
 
-   
+
     DistOutput.innerHTML = DistSlider.value;
-    DistOutput.innerHTML = this.value;
+    
 };
 //diameter slider
 
 
 DiameterSlider.oninput = function () {
 
-    
     DiameterOutput.innerHTML = DiameterSlider.value;
-    DiameterOutput.innerHTML = this.value;
+   
 };
-//Fooot candle slider
+//Foot candle slider
 
 FCSlider.oninput = function () {
-   
+
     FCOutput.innerHTML = FCSlider.value;
-    FCOutput.innerHTML = this.value;
+    
 };
 // document.getElementById("DistSlider").addEventListener("input",upateSliders(this.value) );
 

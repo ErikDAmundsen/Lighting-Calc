a9c6119ce7a997a0b5da0dd9439d97c26f870fda
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-04-05 16:34:25 -0700

    MVP!

--- RefrenceSheet.js
+++ RefrenceSheet.js
@@ -7,15 +7,14 @@ var SM = GetCurrentModel();
 // }
 
 function extractJSON(obj,indent) {
+    var dataBox= document.getElementById("showData");
+    dataBox.innerHTML ="";
     for (const i  in obj) {
-      if (Array.isArray(obj[i]) || typeof obj[i] === 'object') {
-        console.log(indent + i + ' is array or object');
-        extractJSON(obj[i], indent + ' > ' + i + ' > ');
-      } else {
         console.log(indent + i + ': ' + obj[i]);
+        dataBox.innerHTML += (indent + i + ': ' + obj[i]+ "</br>");
       }
     }
-  }
+//   }Addback
   
 //   function extractJSON(obj,indent) {
 //     for (const i  in obj) {

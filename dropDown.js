c746fbc0e4ae8825841574811be0d207af0b6a91
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-05-17 16:37:18 -0700

    formatted

--- dropDown.js
+++ dropDown.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
         newOption.value = opt;
         BrandList.appendChild(newOption);
     };
+    
 });
 // UniqeBrandArray.forEach(function (B) {
 //     var newOption = document.createElement("option");

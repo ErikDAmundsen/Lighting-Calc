2d9b45959f4a1ba9fba15473eaecd19874ed58c5
erikdamundsen@gmail.com <erikdamundsen@gmail.com>
2019-06-04 15:20:49 -0700

    commented out geolocate

--- geolocate.js
+++ geolocate.js
@@ -1,3 +1,25 @@
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(successFunction, failureFunction, {enableHighAccuracy:false, maximumAge:30000, timeout:27000});
-}
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(locateSuccess, locateFail);
+// }
+// function locateSuccess(loc) {
+//     var latitude = loc.coords.latitude;
+//     var longitude = loc.coords.longitude;
+//     alert('You are at ' + latitude + ', ' + longitude);
+// }
+// function locateFail(geoPositionError) {
+//     switch (geoPositionError.code) {
+//         case 0: // UNKNOWN_ERROR
+//             alert('An unknown error occurred, sorry');
+//             break;
+//         case 1: // PERMISSION_DENIED
+//             alert('Permission to use Geolocation was denied');
+//             break;
+//         case 2: // POSITION_UNAVAILABLE
+//             alert('Couldn\'t find you...');
+//             break;
+//         case 3: // TIMEOUT
+//             alert('The Geolocation request took too long and timed out');
+//             break;
+//         default:
+//     }
+// }
